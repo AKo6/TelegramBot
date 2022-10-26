cea43d48b927e19f8945b2aadeca75f90e3e8d67
Александр <93786190+AKo6@users.noreply.github.com>
2022-10-26 21:00:26 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '5530074300:AAFXSWc_abH2nKO_oTX0x6wtiyaG9oormp4'
+keys = {
+    'Рубль': 'RUB',
+    'Доллар': 'USD',
+    'Евро' : 'EUR',
+    'Тенге' : 'KZT',
+    'Лира' : 'TRY',
+    'Лари' : 'GEL',
+    'Биткоин' : 'BTC',
+    'Эфир' : 'ETH'
+}
